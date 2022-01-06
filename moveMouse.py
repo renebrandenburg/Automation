7d4871674757920fb45e908276627414cffcecd3
renebrandenburg <renebrandenburg@gmail.com>
2022-01-06 12:10:26 +0100

    Set duration to 1 hour

--- moveMouse.py
+++ moveMouse.py
@@ -5,3 +5,6 @@ pyautogui.moveTo(100, 150)
 pyautogui.moveRel(0, 10)  # move mouse 10 pixels down
 pyautogui.dragTo(100, 150)
 pyautogui.dragRel(0, 10)  # drag mouse 10 pixels down
+
+
+pyautogui.moveRel(0, 50, duration = 3600)
